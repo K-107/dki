e9460a20724d11effc9f3fc82aa85fcf3cbcfdd9
Jonghee Lee <jonghee89@naver.com>
2022-02-23 11:12:05 +0900

    코드 수정

--- Food_Detection/220214_txt욜로v5형식변환/YOLOv5_txt_generator.py
+++ Food_Detection/220214_txt욜로v5형식변환/YOLOv5_txt_generator.py
@@ -17,13 +17,10 @@ def yolov5_txt_save(txt_name, single_or_multi, list_, case):
     try:      
         if case == '1': # norm이 됐고 좌표가 Center 기준일 경우
             content,class_num, coord_ok = norm_Center(list_)
-        
         elif case == '2': # norm이 됐고 좌표가 TopLeft 기준일 경우
             content,class_num, coord_ok = norm_TopLeft(list_)
-        
         elif case == '3': # norm이 안 됐고 좌표가 center 기준일 경우
             content,class_num, coord_ok = center(list_)
-        
         elif case == '4': # norm이 안 됐고 좌표가 topLeft 기준일 경우
             content,class_num, coord_ok = topLeft(list_)
         
@@ -57,14 +54,16 @@ def norm_Center(list_):
     content = '' # txt에 적을 내용
     coord_ok = 'Y' # 좌표들 중에 음수가 없는 정상이면 Y, 음수가 있어서 비정상이면 N
     for coord in list_:
+        
+        if float(coord[3])<0 or float(coord[4])<0 or float(coord[5])<0 or float(coord[6])<0:
+            coord_ok = 'N'
+            return '', '', coord_ok
+        
         class_num = coord[0]
         x_center = float(coord[3])
         y_center = float(coord[4])
         w_bbox = float(coord[5])
         h_bbox = float(coord[6])
-        
-        if float(coord[3])<0 or float(coord[4])<0 or float(coord[5])<0 or float(coord[6])<0:
-            coord_ok = 'N'
         
         content += str(class_num) + ' '
         content += str(round(x_center, 6)) + ' '
@@ -81,14 +80,16 @@ def norm_TopLeft(list_):
     content = '' # txt에 적을 내용
     coord_ok = 'Y' # 좌표들 중에 음수가 없는 정상이면 Y, 음수가 있어서 비정상이면 N
     for coord in list_:
+        
+        if float(coord[3])<0 or float(coord[4])<0 or float(coord[5])<0 or float(coord[6])<0:
+            coord_ok = 'N'
+            return '', '', coord_ok
+        
         class_num = coord[0]
         x_center = float(coord[3])+float(coord[5])/2
         y_center = float(coord[4])+float(coord[6])/2
         w_bbox = float(coord[5])
         h_bbox = float(coord[6])
-        
-        if float(coord[3])<0 or float(coord[4])<0 or float(coord[5])<0 or float(coord[6])<0:
-            coord_ok = 'N'
             
         content += str(class_num) + ' '
         content += str(round(x_center, 6)) + ' '
@@ -105,14 +106,16 @@ def center(list_):
     content = '' # txt에 적을 내용
     coord_ok = 'Y' # 좌표들 중에 음수가 없는 정상이면 Y, 음수가 있어서 비정상이면 N
     for coord in list_:
+        
+        if float(coord[3])<0 or float(coord[4])<0 or float(coord[5])<0 or float(coord[6])<0:
+            coord_ok = 'N'
+            return '', '', coord_ok
+        
         class_num = coord[0]
         x_center = float(coord[3])/float(coord[1])
         y_center = float(coord[4])/float(coord[2])
         w_bbox = float(coord[5])/float(coord[1])
         h_bbox = float(coord[6])/float(coord[2])
-        
-        if float(coord[3])<0 or float(coord[4])<0 or float(coord[5])<0 or float(coord[6])<0:
-            coord_ok = 'N'
             
         content += str(class_num) + ' '
         content += str(round(x_center, 6)) + ' '
@@ -129,14 +132,16 @@ def topLeft(list_):
     content = '' # txt에 적을 내용
     coord_ok = 'Y' # 좌표들 중에 음수가 없는 정상이면 Y, 음수가 있어서 비정상이면 N
     for coord in list_:
+        
+        if float(coord[3])<0 or float(coord[4])<0 or float(coord[5])<0 or float(coord[6])<0:
+            coord_ok = 'N'
+            return '', '', coord_ok
+        
         class_num = coord[0]
         x_center = (float(coord[3]) + float(coord[5])/2)/float(coord[1])
         y_center = (float(coord[4]) + float(coord[6])/2)/float(coord[2])
         w_bbox = float(coord[5])/float(coord[1])
         h_bbox = float(coord[6])/float(coord[2])
-        
-        if float(coord[3])<0 or float(coord[4])<0 or float(coord[5])<0 or float(coord[6])<0:
-            coord_ok = 'N'
             
         content += str(class_num) + ' '
         content += str(round(x_center, 6)) + ' '
