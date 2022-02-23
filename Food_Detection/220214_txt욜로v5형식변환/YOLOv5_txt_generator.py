0dfc4ec2671b278901258c9aa0fe9cb9449f742c
Jonghee Lee <jonghee89@naver.com>
2022-02-23 14:04:08 +0900

    폴더를 먼저 만들어야지 해당 경로의 파일을 생성할 수 있따!

--- Food_Detection/220214_txt욜로v5형식변환/YOLOv5_txt_generator.py
+++ Food_Detection/220214_txt욜로v5형식변환/YOLOv5_txt_generator.py
@@ -3,32 +3,36 @@ import os
 
 
 def yolov5_txt_save(txt_name, single_or_multi, list_, case):
+    
     '''
     input:
     txt_name -        생성할 txt파일 이름 
     single_or_multi - 레이블이 단일인지 멀티인지
     list_ -           txt에 작성할 좌표가 들어있는 리스트
-    case -            1(norm_Center), 2(norm_TopLeft), 3(center), 4(topLeft)
-    
+    case -            1(norm_TopLeft), 2(norm_Center), 3(topLeft), 4(center)
     output:
     txt_path -        생성된 txt를 저장할 경로
     '''
     
     try:      
-        if case == '1': # norm이 됐고 좌표가 Center 기준일 경우
-            content,class_num, coord_ok = norm_Center(list_)
-        elif case == '2': # norm이 됐고 좌표가 TopLeft 기준일 경우
+        if case == '1': # norm이 됐고 좌표가 TopLeft 기준일 경우
             content,class_num, coord_ok = norm_TopLeft(list_)
-        elif case == '3': # norm이 안 됐고 좌표가 center 기준일 경우
-            content,class_num, coord_ok = center(list_)
-        elif case == '4': # norm이 안 됐고 좌표가 topLeft 기준일 경우
+        elif case == '2': # norm이 됐고 좌표가 Center 기준일 경우
+            content,class_num, coord_ok = norm_Center(list_)
+        elif case == '3': # norm이 안 됐고 좌표가 topLeft 기준일 경우
             content,class_num, coord_ok = topLeft(list_)
+        elif case == '4': # norm이 안 됐고 좌표가 center 기준일 경우
+            content,class_num, coord_ok = center(list_)
         
         if coord_ok == 'Y': # 좌표들 중에 음수가 없는 정상이면 Y
             # txt파일을 저장할 path 설정
             path = '/Data/FoodDetection/data/food_detection/food_detection/yolov5_format'
             if single_or_multi == 'single':
-                txt_path = os.path.join(path, single_or_multi, class_num, txt_name)
+                # 20220223-L  class_num이 int이기 때문에 오류가 나서 str로 수정
+                #txt_path = os.path.join(path, single_or_multi, class_num, txt_name)
+                txt_path = os.path.join(path, single_or_multi, str(class_num))
+                create_output_path(txt_path) # 폴더 생성
+                txt_path = os.path.join(txt_path, txt_name)
             elif single_or_multi == 'multi':
                 txt_path = os.path.join(path, single_or_multi, txt_name)
         
@@ -42,39 +46,23 @@ def yolov5_txt_save(txt_name, single_or_multi, list_, case):
         elif coord_ok == 'N': # 좌표들 중에 음수가 있어서 비정상이면 N
             return '', coord_ok
         
-    except:
-        message = '파일 생성 실패'
+    except Exception as e:
+        #except:    
+        # 20220223-L 오류 코드가 직관적이지 않아서 수정
+        #print('예외가 발생했습니다.', e)
+        #message = '파일 생성 실패'
+        message = e
         print(message)
-        return message, ''
+        return message, 'N'
         
         
-'''case == '1', norm이 됐고 좌표가 Center 기준일 경우'''
-def norm_Center(list_):
-    
-    content = '' # txt에 적을 내용
-    coord_ok = 'Y' # 좌표들 중에 음수가 없는 정상이면 Y, 음수가 있어서 비정상이면 N
-    for coord in list_:
+# 폴더 생성
+def create_output_path(output_path):
+    if not os.path.isdir(output_path):
+        os.mkdir(output_path)
         
-        if float(coord[3])<0 or float(coord[4])<0 or float(coord[5])<0 or float(coord[6])<0:
-            coord_ok = 'N'
-            return '', '', coord_ok
-        
-        class_num = coord[0]
-        x_center = float(coord[3])
-        y_center = float(coord[4])
-        w_bbox = float(coord[5])
-        h_bbox = float(coord[6])
-        
-        content += str(class_num) + ' '
-        content += str(round(x_center, 6)) + ' '
-        content += str(round(y_center, 6)) + ' '
-        content += str(round(w_bbox, 6)) + ' '
-        content += str(round(h_bbox, 6)) + '\n'
-        
-    return content, class_num, coord_ok
 
-
-'''case == '2', norm이 됐고 좌표가 TopLeft 기준일 경우'''
+'''case == '1', norm이 됐고 좌표가 TopLeft 기준일 경우'''
 def norm_TopLeft(list_):
     
     content = '' # txt에 적을 내용
@@ -100,9 +88,9 @@ def norm_TopLeft(list_):
     return content, class_num, coord_ok
 
 
-'''case == '3', norm이 안 됐고 좌표가 center 기준일 경우'''
-def center(list_):
-        
+'''case == '2', norm이 됐고 좌표가 Center 기준일 경우'''
+def norm_Center(list_):
+    
     content = '' # txt에 적을 내용
     coord_ok = 'Y' # 좌표들 중에 음수가 없는 정상이면 Y, 음수가 있어서 비정상이면 N
     for coord in list_:
@@ -112,11 +100,11 @@ def center(list_):
             return '', '', coord_ok
         
         class_num = coord[0]
-        x_center = float(coord[3])/float(coord[1])
-        y_center = float(coord[4])/float(coord[2])
-        w_bbox = float(coord[5])/float(coord[1])
-        h_bbox = float(coord[6])/float(coord[2])
-            
+        x_center = float(coord[3])
+        y_center = float(coord[4])
+        w_bbox = float(coord[5])
+        h_bbox = float(coord[6])
+        
         content += str(class_num) + ' '
         content += str(round(x_center, 6)) + ' '
         content += str(round(y_center, 6)) + ' '
@@ -126,7 +114,7 @@ def center(list_):
     return content, class_num, coord_ok
 
 
-'''case == '4', norm이 안 됐고 좌표가 topLeft 기준일 경우'''
+'''case == '3', norm이 안 됐고 좌표가 topLeft 기준일 경우'''
 def topLeft(list_):
     
     content = '' # txt에 적을 내용
@@ -140,6 +128,32 @@ def topLeft(list_):
         class_num = coord[0]
         x_center = (float(coord[3]) + float(coord[5])/2)/float(coord[1])
         y_center = (float(coord[4]) + float(coord[6])/2)/float(coord[2])
+        w_bbox = float(coord[5])/float(coord[1])
+        h_bbox = float(coord[6])/float(coord[2])
+            
+        content += str(class_num) + ' '
+        content += str(round(x_center, 6)) + ' '
+        content += str(round(y_center, 6)) + ' '
+        content += str(round(w_bbox, 6)) + ' '
+        content += str(round(h_bbox, 6)) + '\n'
+        
+    return content, class_num, coord_ok
+
+
+'''case == '4', norm이 안 됐고 좌표가 center 기준일 경우'''
+def center(list_):
+        
+    content = '' # txt에 적을 내용
+    coord_ok = 'Y' # 좌표들 중에 음수가 없는 정상이면 Y, 음수가 있어서 비정상이면 N
+    for coord in list_:
+        
+        if float(coord[3])<0 or float(coord[4])<0 or float(coord[5])<0 or float(coord[6])<0:
+            coord_ok = 'N'
+            return '', '', coord_ok
+        
+        class_num = coord[0]
+        x_center = float(coord[3])/float(coord[1])
+        y_center = float(coord[4])/float(coord[2])
         w_bbox = float(coord[5])/float(coord[1])
         h_bbox = float(coord[6])/float(coord[2])
             
