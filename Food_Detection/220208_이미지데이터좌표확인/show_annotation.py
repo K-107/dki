faa3b85eb366847905c9f37c7cd3a18da58d1025
Jonghee Lee <jonghee89@naver.com>
2022-02-24 14:23:57 +0900

    euc-kr, utf-8 모두 인코딩 되도록 코드 수정

--- Food_Detection/220208_이미지데이터좌표확인/show_annotation.py
+++ Food_Detection/220208_이미지데이터좌표확인/show_annotation.py
@@ -66,127 +66,263 @@ def imgTxtCheck(img, txt):
         w, h = float(img.size[0]), float(img.size[1])
         #print('img.size: ', w, h)
         
-        with open(txt, "r", encoding='utf-8') as text:
-            lines = text.readlines()
-            if len(lines) > 1:
-                print('멀티 레이블이다!')
-                
-            # 멀티 레이블일 때 멀티 바운딩 박스를 한 이미지에 표시하기 위한 리스트 
-            box = []
-            for num in range(len(lines)):
-                line = lines[num].replace('\n', '')
-                m_line = p.findall(line)
-                #print('txt 내용: ', m_line)
-                
-                # 노말라이제이션 된 것은 길이가 5이다.
-                if len(m_line) == 5:
-                    normalization = True
-                    print('Normalization 완료')
-                    
-                    label, x_coord, y_coord, bbox_w, bbox_h  = \
-                    class_dic[int(m_line[0])], float(m_line[1]), float(m_line[2]), float(m_line[3]), float(m_line[4])
-                    
-                    #print('label, x_coord, y_coord, bbox_w, bbox_h: ', label, x_coord, y_coord, bbox_w, bbox_h)
-                    print('label: ', label)
+        '''
+        2202224 수정 사항
+        utf-8로 인코딩이 안되면 euc-kr로 인코딩하자!
+        try-except로 예외처리한다! try와 except 하위 코드는 같다.
+        '''
+        try: 
+            with open(txt, "r", encoding='utf-8') as text:
+                lines = text.readlines()
+                if len(lines) > 1:
+                    print('멀티 레이블이다!')
 
-                    #좌상단 기준의 x,y일 경우
-                    topLeft = [(x_coord+bbox_w/2)*w, (y_coord+bbox_h/2)*h, bbox_w*w, bbox_h*h, label]
-                                        
-                    # 센터 기준의 x,y일 경우
-                    center = [x_coord*w, y_coord*h, bbox_w*w, bbox_h*h, label]
-                    
-                
-                else:
-                    '''
-                    220209 수정 사항
-                    노말라이제이션 안된 것들은 리스트의 길이가 5가 아니다.
-                    '''
-                    normalization = False
-                    print('Normalization 안됨')
-                    
-                    '''
-                    label, w_txt, h_txt, x_coord, y_coord, bbox_w, bbox_h  = \
-                    m_line[6], float(m_line[0]), float(m_line[1]), float(m_line[2]), \
-                    float(m_line[3]), float(m_line[4]), float(m_line[5])
+                # 멀티 레이블일 때 멀티 바운딩 박스를 한 이미지에 표시하기 위한 리스트 
+                box = []
+                for num in range(len(lines)):
+                    line = lines[num].replace('\n', '')
+                    m_line = p.findall(line)
+                    #print('txt 내용: ', m_line)
 
-                    220209 수정 사항
-                    레이블에 ' ' 띄어쓰기가 있을 경우 문제 해결
-                    '_'.join(m_line[6:])으로 6번 인덱스 이후의 레이블들을 _로 연결해준다.
-                    '''
-                    label, w_txt, h_txt, x_coord, y_coord, bbox_w, bbox_h  = \
-                    '_'.join(m_line[6:]), float(m_line[0]), float(m_line[1]), float(m_line[2]), \
-                    float(m_line[3]), float(m_line[4]), float(m_line[5])
-                    
-                    if w_txt != w or h_txt != h:
-                        print('이미지 사이즈 잘못 측정됨')
-                    
-                    if label == 'None':
-                        label = txt.split('/')[-1]
+                    # 노말라이제이션 된 것은 길이가 5이다.
+                    if len(m_line) == 5:
+                        normalization = True
+                        print('Normalization 완료')
+
+                        label, x_coord, y_coord, bbox_w, bbox_h  = \
+                        class_dic[int(m_line[0])], float(m_line[1]), float(m_line[2]), float(m_line[3]), float(m_line[4])
+
+                        #print('label, x_coord, y_coord, bbox_w, bbox_h: ', label, x_coord, y_coord, bbox_w, bbox_h)
                         print('label: ', label)
-                    
-                        '''
-                        220214 수정사항
-                        레이블이 숫자인 경우 class_dic에서 찾음.
-                        '''
-                    elif label.isdigit( ):
-                        label = class_dic.get(int(label), 'class_dic에 없음')
-                        print('label: ', label)
-                    
+
+                        #좌상단 기준의 x,y일 경우
+                        topLeft = [(x_coord+bbox_w/2)*w, (y_coord+bbox_h/2)*h, bbox_w*w, bbox_h*h, label]
+
+                        # 센터 기준의 x,y일 경우
+                        center = [x_coord*w, y_coord*h, bbox_w*w, bbox_h*h, label]
+
+
                     else:
-                        print('label: ', label)
-                    
-                    #좌상단 기준의 x,y일 경우
-                    topLeft = [x_coord+bbox_w/2, y_coord+bbox_h/2, bbox_w, bbox_h, label]
-                    
-                    # 센터 기준의 x,y일 경우
-                    center = [x_coord, y_coord, bbox_w, bbox_h, label]
-            
-                #print('Top Left 기준일 경우: ', topLeft, '\nCenter 기준일 경우: ', center)
-                
-                '''
-                append로 여러개 한꺼번에 추가할 때는 extend를 사용하자!
-                참고: https://www.codeit.kr/community/threads/12373
-                '''
-                box.extend([topLeft, center])
-        
-            #print(box)
-            n_cols = 2
-            fig, axs = plt.subplots(figsize=(8*n_cols, 8), nrows=1, ncols=n_cols)
+                        '''
+                        220209 수정 사항
+                        노말라이제이션 안된 것들은 리스트의 길이가 5가 아니다.
+                        '''
+                        normalization = False
+                        print('Normalization 안됨')
 
-            print('\nbox 내용: ', box)
-            for ind, coord in enumerate(box):
-                #print(ind, coord)
-                '''Top Left를 왼쪽에 표시하고 Center는 오른쪽에 표시한다.'''
-                if ind % 2==0:
-                    ind = 0
-                    axs[ind].set_title('Coordinate : Top Left')
-                else:
-                    ind = 1
-                    axs[ind].set_title('Coordinate : Center')
+                        '''
+                        label, w_txt, h_txt, x_coord, y_coord, bbox_w, bbox_h  = \
+                        m_line[6], float(m_line[0]), float(m_line[1]), float(m_line[2]), \
+                        float(m_line[3]), float(m_line[4]), float(m_line[5])
+
+                        220209 수정 사항
+                        레이블에 ' ' 띄어쓰기가 있을 경우 문제 해결
+                        '_'.join(m_line[6:])으로 6번 인덱스 이후의 레이블들을 _로 연결해준다.
+                        '''
+                        label, w_txt, h_txt, x_coord, y_coord, bbox_w, bbox_h  = \
+                        '_'.join(m_line[6:]), float(m_line[0]), float(m_line[1]), float(m_line[2]), \
+                        float(m_line[3]), float(m_line[4]), float(m_line[5])
+
+                        if w_txt != w or h_txt != h:
+                            print('이미지 사이즈 잘못 측정됨')
+
+                        if label == 'None':
+                            label = txt.split('/')[-1]
+                            print('label: ', label)
+
+                            '''
+                            220214 수정사항
+                            레이블이 숫자인 경우 class_dic에서 찾음.
+                            '''
+                        elif label.isdigit( ):
+                            label = class_dic.get(int(label), 'class_dic에 없음')
+                            print('label: ', label)
+
+                        else:
+                            print('label: ', label)
+
+                        #좌상단 기준의 x,y일 경우
+                        topLeft = [x_coord+bbox_w/2, y_coord+bbox_h/2, bbox_w, bbox_h, label]
+
+                        # 센터 기준의 x,y일 경우
+                        center = [x_coord, y_coord, bbox_w, bbox_h, label]
+
+                    #print('Top Left 기준일 경우: ', topLeft, '\nCenter 기준일 경우: ', center)
+
+                    '''
+                    append로 여러개 한꺼번에 추가할 때는 extend를 사용하자!
+                    참고: https://www.codeit.kr/community/threads/12373
+                    '''
+                    box.extend([topLeft, center])
+
+                #print(box)
+                n_cols = 2
+                fig, axs = plt.subplots(figsize=(8*n_cols, 8), nrows=1, ncols=n_cols)
+
+                print('\nbox 내용: ', box)
+                for ind, coord in enumerate(box):
+                    #print(ind, coord)
+                    '''Top Left를 왼쪽에 표시하고 Center는 오른쪽에 표시한다.'''
+                    if ind % 2==0:
+                        ind = 0
+                        axs[ind].set_title('Coordinate : Top Left')
+                    else:
+                        ind = 1
+                        axs[ind].set_title('Coordinate : Center')
+
+                    Korim = np.array(img, dtype=np.uint8)
+                    axs[ind].imshow(Korim)
+
+                    newPt = [[coord[0]-coord[2]/2, coord[1]+coord[3]/2], [coord[0]+coord[2]/2, coord[1]+coord[3]/2], \
+                             [coord[0]+coord[2]/2, coord[1]-coord[3]/2], [coord[0]-coord[2]/2, coord[1]-coord[3]/2]]
+
+                    # axs.text(1.05, 10.05, coord[4], fontsize=30, horizontalalignment='left', verticalalignment='bottom', bbox=props, fontproperties=font_name)
+                    # https://runebook.dev/ko/docs/matplotlib/_as_gen/matplotlib.axes.axes.text
+                    props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+                    # x = coord[0]-center_X, y = coord[1]-center_Y, s = coord[4]-label
+                    axs[ind].text(coord[0], coord[1], coord[4], fontsize=15, horizontalalignment='center', verticalalignment='center', bbox=props, fontproperties=font_name)    
+                    rect = Polygon(newPt, linewidth=1.5, edgecolor='r', facecolor='none')
+                    axs[ind].add_patch(rect)
+
+                # 좌표확인_{label}.png 이름 저장
+                output_name = f'./좌표확인_{label}.png'
+                plt.savefig(output_name)
+
+                # 현재 파일/디렉토리 위치 확인 및 변경
+                # https://velog.io/@stu_dy/Python-%ED%98%84%EC%9E%AC-%ED%8C%8C%EC%9D%BC%EB%94%94%EB%A0%89%ED%86%A0%EB%A6%AC-%EC%9C%84%EC%B9%98-%ED%99%95%EC%9D%B8-%EB%B0%8F-%EB%B3%80%EA%B2%BD
+                output_path = os.path.realpath(f'./좌표확인_{label}.png')
+                print('\n좌표확인 이미지 저장 위치: ', output_path)
+
+                return output_name, output_path, box, normalization
         
-                Korim = np.array(img, dtype=np.uint8)
-                axs[ind].imshow(Korim)
+            '''2202224 수정 사항
+        utf-8로 인코딩이 안되면 euc-kr로 인코딩하자!
+        try-except로 예외처리한다!'''
+        except: 
+
+            with open(txt, "r", encoding='euc-kr') as text:
+                lines = text.readlines()
+                if len(lines) > 1:
+                    print('멀티 레이블이다!')
+
+                # 멀티 레이블일 때 멀티 바운딩 박스를 한 이미지에 표시하기 위한 리스트 
+                box = []
+                for num in range(len(lines)):
+                    line = lines[num].replace('\n', '')
+                    m_line = p.findall(line)
+                    #print('txt 내용: ', m_line)
+
+                    # 노말라이제이션 된 것은 길이가 5이다.
+                    if len(m_line) == 5:
+                        normalization = True
+                        print('Normalization 완료')
+
+                        label, x_coord, y_coord, bbox_w, bbox_h  = \
+                        class_dic[int(m_line[0])], float(m_line[1]), float(m_line[2]), float(m_line[3]), float(m_line[4])
+
+                        #print('label, x_coord, y_coord, bbox_w, bbox_h: ', label, x_coord, y_coord, bbox_w, bbox_h)
+                        print('label: ', label)
+
+                        #좌상단 기준의 x,y일 경우
+                        topLeft = [(x_coord+bbox_w/2)*w, (y_coord+bbox_h/2)*h, bbox_w*w, bbox_h*h, label]
+
+                        # 센터 기준의 x,y일 경우
+                        center = [x_coord*w, y_coord*h, bbox_w*w, bbox_h*h, label]
+
+
+                    else:
+                        '''
+                        220209 수정 사항
+                        노말라이제이션 안된 것들은 리스트의 길이가 5가 아니다.
+                        '''
+                        normalization = False
+                        print('Normalization 안됨')
+
+                        '''
+                        label, w_txt, h_txt, x_coord, y_coord, bbox_w, bbox_h  = \
+                        m_line[6], float(m_line[0]), float(m_line[1]), float(m_line[2]), \
+                        float(m_line[3]), float(m_line[4]), float(m_line[5])
+
+                        220209 수정 사항
+                        레이블에 ' ' 띄어쓰기가 있을 경우 문제 해결
+                        '_'.join(m_line[6:])으로 6번 인덱스 이후의 레이블들을 _로 연결해준다.
+                        '''
+                        label, w_txt, h_txt, x_coord, y_coord, bbox_w, bbox_h  = \
+                        '_'.join(m_line[6:]), float(m_line[0]), float(m_line[1]), float(m_line[2]), \
+                        float(m_line[3]), float(m_line[4]), float(m_line[5])
+
+                        if w_txt != w or h_txt != h:
+                            print('이미지 사이즈 잘못 측정됨')
+
+                        if label == 'None':
+                            label = txt.split('/')[-1]
+                            print('label: ', label)
+
+                            '''
+                            220214 수정사항
+                            레이블이 숫자인 경우 class_dic에서 찾음.
+                            '''
+                        elif label.isdigit( ):
+                            label = class_dic.get(int(label), 'class_dic에 없음')
+                            print('label: ', label)
+
+                        else:
+                            print('label: ', label)
+
+                        #좌상단 기준의 x,y일 경우
+                        topLeft = [x_coord+bbox_w/2, y_coord+bbox_h/2, bbox_w, bbox_h, label]
+
+                        # 센터 기준의 x,y일 경우
+                        center = [x_coord, y_coord, bbox_w, bbox_h, label]
+
+                    #print('Top Left 기준일 경우: ', topLeft, '\nCenter 기준일 경우: ', center)
+
+                    '''
+                    append로 여러개 한꺼번에 추가할 때는 extend를 사용하자!
+                    참고: https://www.codeit.kr/community/threads/12373
+                    '''
+                    box.extend([topLeft, center])
+
+                #print(box)
+                n_cols = 2
+                fig, axs = plt.subplots(figsize=(8*n_cols, 8), nrows=1, ncols=n_cols)
+
+                print('\nbox 내용: ', box)
+                for ind, coord in enumerate(box):
+                    #print(ind, coord)
+                    '''Top Left를 왼쪽에 표시하고 Center는 오른쪽에 표시한다.'''
+                    if ind % 2==0:
+                        ind = 0
+                        axs[ind].set_title('Coordinate : Top Left')
+                    else:
+                        ind = 1
+                        axs[ind].set_title('Coordinate : Center')
+
+                    Korim = np.array(img, dtype=np.uint8)
+                    axs[ind].imshow(Korim)
+
+                    newPt = [[coord[0]-coord[2]/2, coord[1]+coord[3]/2], [coord[0]+coord[2]/2, coord[1]+coord[3]/2], \
+                             [coord[0]+coord[2]/2, coord[1]-coord[3]/2], [coord[0]-coord[2]/2, coord[1]-coord[3]/2]]
+
+                    # axs.text(1.05, 10.05, coord[4], fontsize=30, horizontalalignment='left', verticalalignment='bottom', bbox=props, fontproperties=font_name)
+                    # https://runebook.dev/ko/docs/matplotlib/_as_gen/matplotlib.axes.axes.text
+                    props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+                    # x = coord[0]-center_X, y = coord[1]-center_Y, s = coord[4]-label
+                    axs[ind].text(coord[0], coord[1], coord[4], fontsize=15, horizontalalignment='center', verticalalignment='center', bbox=props, fontproperties=font_name)    
+                    rect = Polygon(newPt, linewidth=1.5, edgecolor='r', facecolor='none')
+                    axs[ind].add_patch(rect)
+
+                # 좌표확인_{label}.png 이름 저장
+                output_name = f'./좌표확인_{label}.png'
+                plt.savefig(output_name)
+
+                # 현재 파일/디렉토리 위치 확인 및 변경
+                # https://velog.io/@stu_dy/Python-%ED%98%84%EC%9E%AC-%ED%8C%8C%EC%9D%BC%EB%94%94%EB%A0%89%ED%86%A0%EB%A6%AC-%EC%9C%84%EC%B9%98-%ED%99%95%EC%9D%B8-%EB%B0%8F-%EB%B3%80%EA%B2%BD
+                output_path = os.path.realpath(f'./좌표확인_{label}.png')
+                print('\n좌표확인 이미지 저장 위치: ', output_path)
+
+                return output_name, output_path, box, normalization
             
-                newPt = [[coord[0]-coord[2]/2, coord[1]+coord[3]/2], [coord[0]+coord[2]/2, coord[1]+coord[3]/2], \
-                         [coord[0]+coord[2]/2, coord[1]-coord[3]/2], [coord[0]-coord[2]/2, coord[1]-coord[3]/2]]
-                
-                # axs.text(1.05, 10.05, coord[4], fontsize=30, horizontalalignment='left', verticalalignment='bottom', bbox=props, fontproperties=font_name)
-                # https://runebook.dev/ko/docs/matplotlib/_as_gen/matplotlib.axes.axes.text
-                props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
-                # x = coord[0]-center_X, y = coord[1]-center_Y, s = coord[4]-label
-                axs[ind].text(coord[0], coord[1], coord[4], fontsize=15, horizontalalignment='center', verticalalignment='center', bbox=props, fontproperties=font_name)    
-                rect = Polygon(newPt, linewidth=1.5, edgecolor='r', facecolor='none')
-                axs[ind].add_patch(rect)
-                
-            # 좌표확인_{label}.png 이름 저장
-            output_name = f'./좌표확인_{label}.png'
-            plt.savefig(output_name)
-                
-            # 현재 파일/디렉토리 위치 확인 및 변경
-            # https://velog.io/@stu_dy/Python-%ED%98%84%EC%9E%AC-%ED%8C%8C%EC%9D%BC%EB%94%94%EB%A0%89%ED%86%A0%EB%A6%AC-%EC%9C%84%EC%B9%98-%ED%99%95%EC%9D%B8-%EB%B0%8F-%EB%B3%80%EA%B2%BD
-            output_path = os.path.realpath(f'./좌표확인_{label}.png')
-            print('\n좌표확인 이미지 저장 위치: ', output_path)
-            
-            return output_name, output_path, box, normalization
+
     except:
         return print(f'경로가 잘못 되었다!!\nimg 존재: {os.path.isfile(img)}, txt 존재: {os.path.isfile(txt)}')
